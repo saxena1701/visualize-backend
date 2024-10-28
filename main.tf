f87145bf044bdf0c11722d4f2bb0564e8736fbc6
Akshat Saxena <143452326+saxena1701@users.noreply.github.com>
2024-10-28 14:44:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "docker_builder" {
   ami                    = "ami-06b21ccaeff8cd686"  
   instance_type          = "t2.micro"
   key_name               = "deploy-server-keys"         
-  user_data              = file("${path.module}/ec2_docker_setup.sh")
+  user_data              = file("./ec2_docker_setup.sh")
   lifecycle {
     create_before_destroy = true
   }
