e673d7c2fe8bf7c015d67f6b953cd3b9da826ed4
Akshat Saxena <143452326+saxena1701@users.noreply.github.com>
2024-10-28 13:55:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "docker_builder" {
-  ami                    = "ami-0c55b159cbfafe1f0"  
+  ami                    = "ami-06b21ccaeff8cd686"  
   instance_type          = "t2.micro"
   key_name               = "deploy-server-keys"         
   user_data              = file("${path.module}/ec2_docker_setup.sh")
